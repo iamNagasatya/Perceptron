6e07c08fbf4f035792a4840ada7d92236f0fb62f
Satyad3v <mrcool145@outlook.com>
2023-05-22 19:52:28 +0530

    or model created with more modularity

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ from utils.model import Perceptron
 from utils.all_utils import prepare_data
 import pandas as pd
 
-model = Perceptron(eta=0.1, epochs=10)
 
-df = pd.DataFrame({"x1":[0, 0, 1, 1], "x2" : [0, 1, 0, 1], "y" : [0, 0, 0, 1]})
+def main(data, model_name="or.model", eta=0.1, epochs=10, model_dir="Perceptron Models"):
+    df = pd.DataFrame(data)
+    model = Perceptron(eta, epochs)
 
-x, y = prepare_data(df, target_col="y")
+    x, y = prepare_data(df, target_col="y")
 
-model.fit(x, y)
+    model.fit(x, y)
 
-print(f"Prediction for > 0, 1 is {model.predict([0, 1])}")
+    print(f"Prediction for > 0, 1 is {model.predict([0, 1])}")
 
-print(f"Total loss is {model.total_loss}")
+    print(f"Total loss is {model.total_loss}")
 
-#saving model
+    #saving model
 
-model.save("perceptron_or_gate.model", "Perceptron Models")
+    model.save(model_name, model_dir)
 
+
+if __name__  == "__main__":
+    data = {"x1":[0, 0, 1, 1], "x2" : [0, 1, 0, 1], "y" : [0, 0, 0, 1]}
+    main(data)
