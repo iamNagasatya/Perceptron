462a2edd35977d745f69fe18ede53fe314be6435
Nagasatya Sai Nallam <n.nagasatyasai@gmail.com>
2023-05-22 21:01:37 +0530

    Documented the code with docstring

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -1,6 +1,15 @@
 import logging
 
 def prepare_data(df, target_col="y"):
+    """It returns features and label for the given dataset
+
+    Args:
+        df (pd.DataFrame): This is a dataframe
+        target_col (str, optional): label or targel column. Defaults to "y".
+
+    Returns:
+        tuple: features and label
+    """
     logging.info("preparing data for training")
     x = df.drop(columns=target_col)
     y = df[target_col]
